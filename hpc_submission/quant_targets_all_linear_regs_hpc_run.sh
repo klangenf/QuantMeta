784a9b82cfe73873d3a37955e39fd66eb6afb25b
klangenf <[mailto:klangenf@umich.edu]>
2022-12-15 12:37:24 -0500

    Adding hpc_submission script for Snakefile-quant_targets_all_linear_regs

---
+++ hpc_submission/quant_targets_all_linear_regs_hpc_run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=quantmeta
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2000mb
+#SBATCH --time=03:00:00
+
+# Account
+#SBATCH --account=duhaimem1 ###Update to hpc specifications
+#SBATCH --partition=standard ###Update to hpc specifications
+
+# Logs
+#SBATCH --mail-user=klangenf@umich.edu ###Update to be use specific
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=Logs/%x-%j.out
+
+# Environment
+##SBATCH --export=ALL
+
+###Before running, have snakemake conda environment active by running "conda activate snakemake"
+source /etc/profile.d/http_proxy.sh
+
+# List compute nodes allocated to the job
+if [[ $SLURM_JOB_NODELIST ]] ; then
+    echo "Running on"
+    scontrol show hostnames $SLURM_JOB_NODELIST
+    echo -e "\n"
+fi
+
+
+
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
+# Initiating snakemake and running workflow in cluster mode
+DIR='/home/klangenf/anaconda/envs' ###Update to user specific anaconda environment path
+snakemake --profile Config --latency-wait 300 --use-conda --conda-prefix $DIR --conda-frontend mamba --snakefile Snakefile-quant_targets_all_linear_regs
